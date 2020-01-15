922d12f3b1875238ccd522ff9b4abb6e8e3b34c4
Paul Lee <paullee20204@gmail.com>
2020-01-16 01:53:16 +0900

    init2

---
+++ askcompany/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from django.http import HttpResponse
+
+def mysum(request,x,y):
+    result=x+y
+    return HttpResponse('result={}'.format(result))
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('mysum/<int:x>/<int:y>/',mysum),
+]
